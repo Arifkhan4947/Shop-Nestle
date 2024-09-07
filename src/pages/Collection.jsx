a1f82f1f142924b96ebccfbdebeb16d97b68be76
Arif Khan <khanarif2738@gmail.com>
2024-09-07 23:10:11 +0530

    Collection && Product component

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -39,6 +39,10 @@ const Collection = () => {
   const applyFilter = () => {
 
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+    }
     
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -76,7 +80,7 @@ const Collection = () => {
   // This useEffect is use for display the data once.
   useEffect(() => {
     applyFilter();
-  },[category,subCategory]);
+  },[category,subCategory, search, showSearch]);
 
 
   // This is useEffect is use for display sort-price data once.

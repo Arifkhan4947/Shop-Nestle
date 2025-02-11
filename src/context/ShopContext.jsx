c768d9d26dbe63ad7af7bf0d1002832743f01c0e
Arif Khan <khanarif2738@gmail.com>
2025-02-11 23:24:26 +0530

    integrate with placeorder

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -168,7 +168,7 @@ const ShopContextProvider = (props) => {
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart,
+        cartItems, addToCart, setCartItems,
         getCartCount, updateQuantity, 
         getCartAmount, navigate, backendUrl,
         setToken,token
